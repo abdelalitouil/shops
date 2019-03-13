372904ff89edf8e358ff5e41023983a01a472275
Abdelali TOUIL <touil.abdelali@gmail.com>
2019-03-13 08:49:59 +0100

    Add function that calculate distance.

--- symfony4/src/Repository/ShopRepository.php
+++ symfony4/src/Repository/ShopRepository.php
@@ -65,4 +65,26 @@ class ShopRepository extends ServiceEntityRepository
         }
         return $shopsArray;
     }
+
+    /**
+     * Calculate the distance between two points a and b
+     * 
+     * @param object $a
+     * @param object $b
+     * @return int
+     */
+    private function distance(object $a, object $b): int
+    {
+        if (($a->latitude == $b->latitude) && ($a->longitude == $b->longitude)) {
+            return 0;
+        } else {
+            $theta = $a->longitude - $b->longitude;
+            $dist = sin(deg2rad($a->latitude)) * sin(deg2rad($b->latitude)) +  cos(deg2rad($a->latitude)) * cos(deg2rad($b->latitude)) * cos(deg2rad($theta));
+            $dist = acos($dist);
+            $dist = rad2deg($dist);
+            $miles = $dist * 60 * 1.1515 * 0.8684;
+
+            return (int) $miles;
+        }
+    }
 }
