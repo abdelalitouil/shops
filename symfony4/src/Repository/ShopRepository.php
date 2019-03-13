c3da15c5684e237336d5c36d23eb222057c7dbe7
Abdelali TOUIL <touil.abdelali@gmail.com>
2019-03-13 08:47:08 +0100

    Add get nearby shops function into Shop repository.

--- symfony4/src/Repository/ShopRepository.php
+++ symfony4/src/Repository/ShopRepository.php
@@ -11,4 +11,42 @@ class ShopRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Shop::class);
     }
+
+    /**
+     * Convert Shop entity to an array.
+     *
+     * @param Shop $shop
+     * @return array
+     */
+    public function toArray(Shop $shop): ?array
+    {
+        return [
+            'id' => $shop->getId(),
+            'name' => $shop->getName(),
+            'location' => $shop->getLocation(),
+            'image' => $shop->getImage(),
+        ];
+    }
+
+    /**
+     * Returns the shop entities as a table.
+     *
+     * @param User $user
+     * @return array
+     */
+    public function getShops($user): ?array
+    {
+        $shops = $this->findAll();
+        $preferredShops = $user->getPreferredShops();
+        $dislikedShops = $user->getDislikedShops();
+
+        $shopsArray = [];
+        foreach ($shops as $shop){
+            // Get all the shops except those who are in the preferred and disliked list
+            if (!$preferredShops->contains($shop) && !$dislikedShops->contains($shop)){
+                $shopsArray[] = $this->toArray($shop);
+            }
+        }
+        return $shopsArray;
+    }
 }
